311241945152d9241ef4e0243657a73d184538b7
Orlando <lopezorlando23@gmail.com>
2020-12-16 10:10:52 -0700

    Create Item Controller

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ApplicationDbContext _context;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
